bfe8117c1c55731a20178130c6ea74d620da8f12
adamghill <adamghill@yahoo.com>
2023-01-16 09:16:05 -0500

    Add some random examples.

--- static/js/script.js
+++ static/js/script.js
@@ -18,9 +18,71 @@ function search(term) {
   }
 }
 
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
   var $search = document.getElementById("search");
   var $result = document.getElementById("result");
+
+  function getSearchTermFromHash() {
+    let searchTerm = location.hash.slice(1);
+    searchTerm = searchTerm.replace("%20", " ");
+
+    return searchTerm;
+  }
+
+  function updateSearchFromHash() {
+    let searchTerm = getSearchTermFromHash();
+
+    $search.value = searchTerm;
+    const result = search(searchTerm);
+
+    if (result) {
+      $result.innerHTML = result;
+      $result.style.color = "var(--success-color)";
+    }
+  }
+
+  function refreshExamples() {
+    var randomExamples = [];
+    var dataKeys = Object.keys(data);
+
+    while (randomExamples.length < 3) {
+      var key = dataKeys[Math.floor(Math.random() * dataKeys.length)];
+      var included = false;
+
+      for (var i = 0; i < randomExamples.length; i++) {
+        if (randomExamples[i] == key) {
+          included = true;
+        }
+      }
+
+      if (location.hash) {
+        let searchTerm = getSearchTermFromHash();
+
+        if (searchTerm === key) {
+          included = true;
+        }
+      }
+
+      if (!included) {
+        randomExamples.push(key);
+      }
+    }
+
+    if (randomExamples) {
+      var examplesHtml = "Some examples: ";
+
+      randomExamples.forEach((key) => {
+        examplesHtml += `<a href="#${key}">${key}</a>, `;
+      });
+
+      examplesHtml = examplesHtml.slice(0, -2);
+      examplesHtml +=
+        ". Add your own by <a href='https://github.com/adamghill/isitwebscale'>forking the repo</a>.";
+
+      var $examples = document.getElementById("examples");
+      $examples.innerHTML = examplesHtml;
+    }
+  }
 
   (function () {
     fetch("/data.json")
@@ -28,20 +90,15 @@ window.addEventListener("load", function () {
       .then((_) => {
         data = _;
 
-        if (location.hash) {
-          const searchTerm = location.hash.slice(1);
-          $search.value = searchTerm;
-          const result = search(searchTerm);
+        refreshExamples();
 
-          if (result) {
-            $result.innerHTML = result;
-            $result.style.color = "var(--success-color)";
-          }
+        if (location.hash) {
+          updateSearchFromHash();
         }
       });
   })();
 
-  $search.addEventListener("input", function () {
+  $search.addEventListener("input", () => {
     const searchTerm = $search.value;
     $result.style.color = "#666";
 
@@ -53,6 +110,8 @@ window.addEventListener("load", function () {
         $result.style.color = "var(--success-color)";
 
         history.pushState({}, "", `#${searchTerm}`);
+
+        refreshExamples();
       } else {
         $result.innerHTML = `<em>${searchTerm}</em> can't be web scale because I've never even heard of it.`;
       }
@@ -61,5 +120,10 @@ window.addEventListener("load", function () {
         emptySpaces[Math.floor(Math.random() * emptySpaces.length)];
       $result.innerHTML = `${emptySpace} web scale and neither is an empty string.`;
     }
+  });
+
+  addEventListener("hashchange", () => {
+    updateSearchFromHash();
+    refreshExamples();
   });
 });
