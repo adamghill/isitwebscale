e427d538b6e159f78d75de21b252cdf6673ca0b5
adamghill <adamghill@yahoo.com>
2023-01-15 21:29:26 -0500

    Spicy progress spinner.

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,14 @@
 let data = {};
 const emptySpaces = ["Black holes aren't", "The void isn't"];
 
-function loadData() {
-  fetch("/data.json")
-    .then((response) => response.json())
-    .then((_data) => {
-      data = _data;
-    });
-}
-loadData();
-
 function search(term) {
+  var $spinner = document.getElementById("spinner");
+  $spinner.style.display = "inline-block";
+
+  setTimeout(() => {
+    $spinner.style.display = "none";
+  }, 500);
+
   term = term.toLowerCase();
 
   for (let key in data) {
@@ -24,14 +22,37 @@ window.addEventListener("load", function () {
   var $search = document.getElementById("search");
   var $result = document.getElementById("result");
 
+  (function () {
+    fetch("/data.json")
+      .then((response) => response.json())
+      .then((_) => {
+        data = _;
+
+        if (location.hash) {
+          const searchTerm = location.hash.slice(1);
+          $search.value = searchTerm;
+          const result = search(searchTerm);
+
+          if (result) {
+            $result.innerHTML = result;
+            $result.style.color = "var(--success-color)";
+          }
+        }
+      });
+  })();
+
   $search.addEventListener("input", function () {
     const searchTerm = $search.value;
+    $result.style.color = "#666";
 
     if (searchTerm.trim()) {
       const result = search(searchTerm);
 
       if (result) {
         $result.innerHTML = result;
+        $result.style.color = "var(--success-color)";
+
+        history.pushState({}, "", `#${searchTerm}`);
       } else {
         $result.innerHTML = `<em>${searchTerm}</em> can't be web scale because I've never even heard of it.`;
       }
